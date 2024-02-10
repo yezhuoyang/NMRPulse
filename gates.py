5a6b3e3de3affa1c7684061c8644f04e60138be3
Zhuoyang Ye <yezhuoyang98@gmail.com>
2024-02-10 14:30:17 -0800

    [Files] Add first file gates.py

---
+++ gates.py
@@ -0,0 +1,17 @@
+import numpy as np
+from qutip import sigmaz
+from qutip.qip.device import Processor
+from qutip.qip.pulse import Pulse
+from qutip.qip.device import LinearSpinChain
+from qutip.qip.circuit import QubitCircuit
+
+
+
+
+if __name__ == "__main__":
+    qc = QubitCircuit(2)
+    qc.add_gate("X", targets=0)
+    processor = LinearSpinChain(2)
+    processor.load_circuit(qc)
+    fig, axis = processor.plot_pulses()
+    fig.show()
